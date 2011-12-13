7a66e2e04a7562d40dc609974238cd103c813889
Sergio Díaz <sendorf@tyrion.(none)>
2011-12-13 16:52:49 +0100

    Open Classes

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -2,9 +2,10 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
+
+class DiceSet
 #   code ...
-# end
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
